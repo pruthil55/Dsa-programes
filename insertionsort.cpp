305431e2c1003015a8ff1a69decdbf47069dd586
Pruthil hirpara <pruthilhirpara9909283353@gmail.com>
2020-01-13 20:00:15 +0530

    Add files via upload

---
+++ insertionsort.cpp
@@ -0,0 +1,29 @@
+#include<iostream.h>
+void main()
+{
+	int size, arr[50], i, j, temp;
+	cout<<"Enter Array Size : ";
+	cin>>size;
+	cout<<"Enter Array Elements : ";
+	for(i=0; i<size; i++)
+	{
+		cin>>arr[i];
+	}
+	cout<<"Sorting array using selection sort ... \n";
+	for(i=1; i<size; i++)
+	{
+		temp=arr[i];
+		j=i-1;
+		while((temp<arr[j]) && (j>=0))
+		{
+			arr[j+1]=arr[j];
+			j=j-1;
+		}
+		arr[j+1]=temp;
+	}
+	cout<<"Array after sorting : \n";
+	for(i=0; i<size; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
